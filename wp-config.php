ded3bba39b0d1b57150b6f199c6ea4495492157f
Teake <teake@patelle.nl>
2018-09-18 17:07:36 +0200

    leave empty

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'coco-nuts');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'nuts');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'nuts2018');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
